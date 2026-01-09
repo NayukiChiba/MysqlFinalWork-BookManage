9a35e3f93d9a93f84f6f9169300d8387cd2e9e41
NayukiChiba <MekoNayuki@outlook.com>
2026-01-09 13:21:41 +0800

    feat: 添加图书搜索路由，支持多种搜索方式

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
+const borrowController = require('../controllers/borrowController');
 
 // 获取所有图书
 router.get('/', bookController.getAllBooks);
+
+// 借书和还书 - 需要放在 /:id 之前
+router.post('/borrow', borrowController.borrowBook);
+router.post('/return', borrowController.returnBook);
+
+// 搜索图书 - 放在 /:id 之前，避免被匹配
+router.get('/search/:query', bookController.searchBooks);
+router.get('/search/name/:query', bookController.searchBooks);
+router.get('/search/author/:query', bookController.searchBooks);
+router.get('/search/tag/:query', bookController.searchBooks);
+router.get('/search/publisher/:query', bookController.searchBooks);
+router.get('/search/isbn/:query', bookController.searchBooks);
 
 // 根据ID获取图书
 router.get('/:id', bookController.getBookById);
@@ -16,8 +29,5 @@ router.put('/:id', bookController.updateBook);
 
 // 删除图书
 router.delete('/:id', bookController.deleteBook);
-
-// 搜索图书
-router.get('/search/:query', bookController.searchBooks);
 
 module.exports = router;
